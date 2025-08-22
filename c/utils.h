0a603e68678b542e10905e7f88f0141e9373424e
cameronrfleming <cameronrfleming@gmail.com>
2025-08-22 11:31:10 +0100

    Create utils.h

---
+++ c/utils.h
@@ -0,0 +1,9 @@
+#ifndef TSETLIN_SPLIT_UTILS
+#define TSETLIN_SPLIT_UTILS
+
+#include <stdio.h>
+
+void read_samples_file(FILE* fp, int num_samples, int num_features, unsigned int* X_data, int* y_data);
+
+#endif
+
